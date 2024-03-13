c68e41f9b1533706d79b81f099374cf8f4892ddb
Christopher Rodriguez <chrissito.rodriguez@gmail.com>
2024-03-13 16:33:38 -0400

    Completed Software engineer file

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,8 +1,20 @@
-class SoftwareEngineer {
+const Employees = require('./Employees.js');
 
+class SoftwareEngineer extends Employees{
+    #programmingLanguages;
 
+    constructor(name, id){
+        super(name, id);
+        this.#programmingLanguages = [];
+    }
 
-    
+    getProgrammingLanguages(){
+        return [...this.#programmingLanguages];
+    }
+
+    setProgrammingLanguage(language){
+        return this.#programmingLanguages.push(language);
+    }
 }
 
 module.exports = {
